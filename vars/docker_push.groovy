2df7a9694dae4e6f5d97b98dcc9c58549e316817
ANSH TIWARI <ansht4919@gmail.com>
2026-01-13 01:09:46 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,8 @@
 def call(String projectname , String imagetag , String credId){
   withCredentials([usernamePassword(credentialsId:credId , passwordVariable:'dockerpass' , usernameVariable:'dockeruser')]){
-    sh "docker login -u ${dockeruser} -p ${dockerpass} "
+    sh "docker login -u ${env.dockeruser} -p ${env.dockerpass} "
   }
-  sh "docker push ${dockeruser}/${projectname}:${imagetag} "
+  sh "docker push ${env.dockeruser}/${projectname}:${imagetag} "
 }
 
   
